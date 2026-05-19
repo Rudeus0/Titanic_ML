13e969e4750d3e0e744f6f661a566734405b9f5e
Ruthresh <rtrwaran5@gmail.com>
2026-05-19 11:22:14 +0530

    feat: add random forest classifier evaluation to model_evaluate
    - Extend function to evaluate RFC model alongside logistic regression
    - Computes accuracy, confusion matrix, classification report, and ROC-AUC for RFC

--- src/evaluate.py
+++ src/evaluate.py
@@ -19,4 +19,16 @@ def model_evaluate(model_log, model_rfc, X_test_scaled, y_test):
     
 
     
-   
+    y_pred_rfc = model_rfc.predict(X_test_scaled)
+
+    accuracy = accuracy_score(y_test, y_pred_rfc)
+    print(f"\n Accuracy:{accuracy:.4f} \n")
+
+    cm = confusion_matrix(y_test, y_pred_rfc)
+    print(f"\n Confusion:{cm} \n ")
+
+    cr = classification_report(y_test, y_pred_rfc)
+    print(f" \n Classificatioin Report:{ cr} \n")
+
+    roc = roc_auc_score(y_test, model_rfc.predict_proba(X_test_scaled)[:, 1])
+    print(f" \n Roc:{roc: .4f} ")
